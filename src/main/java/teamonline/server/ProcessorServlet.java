4131e30e8c3d64d3f252d421d97ce76042ebe362
Faran Negarestan <faran.negarestan@gmail.com>
2017-04-14 14:48:25 -0400

    work in progress

--- src/main/java/teamonline/server/ProcessorServlet.java
+++ src/main/java/teamonline/server/ProcessorServlet.java
@@ -3,6 +3,7 @@ package teamonline.server;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
@@ -11,8 +12,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.stanford.nlp.classify.Classifier;
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.crf.CRFClassifier;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.Word;
 import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.util.CoreMap;
 
 public class ProcessorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -29,16 +35,29 @@ public class ProcessorServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String text = request.getParameter("text");
 		String[] items = text.split(Pattern.quote("||"));
+		StringBuffer tokenized = new StringBuffer();
+		
+		// Tokenizing
 		for(String item: items) {
+			item = item.replace('-', ' ');
 			Reader reader = new StringReader(item);
 			PTBTokenizer<Word> tokenizer = PTBTokenizer.newPTBTokenizer(reader);
 			Word nextToken = null;
-			while( tokenizer.hasNext()) {
+			while(tokenizer.hasNext()) {
 				nextToken = tokenizer.next();
-				System.out.println(nextToken.word());
+				tokenized.append(nextToken.word());
 			}
 		}
-		request.getSession(true).setAttribute("text", text);
+		// Classification
+		String serializedClassifier = "dell_first_25_data_ner-model.ser.gz";
+		try {
+			AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
+			List<List<CoreLabel>> out = classifier.classify(tokenized.toString());
+			System.out.println(out);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		request.getSession(true).setAttribute("text", tokenized.toString());
 	}
 
 }
