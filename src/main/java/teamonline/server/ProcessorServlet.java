3594957138b19111f45ddbee0a6ea3e1a2cb443c
faran <faran@faran-desktop>
2017-04-18 22:34:19 -0400

    work in progress

--- src/main/java/teamonline/server/ProcessorServlet.java
+++ src/main/java/teamonline/server/ProcessorServlet.java
@@ -3,6 +3,7 @@ package teamonline.server;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -33,9 +34,11 @@ public class ProcessorServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Processing started...");
 		String text = request.getParameter("text");
 		String[] items = text.split(Pattern.quote("||"));
 		StringBuffer tokenized = new StringBuffer();
+		ArrayList<CoreLabel> tokens = new ArrayList<CoreLabel>();
 		
 		// Tokenizing
 		for(String item: items) {
@@ -46,14 +49,19 @@ public class ProcessorServlet extends HttpServlet {
 			while(tokenizer.hasNext()) {
 				nextToken = tokenizer.next();
 				tokenized.append(nextToken.word());
+				System.out.println(nextToken.word());
+				tokens.add(new CoreLabel(nextToken));
 			}
 		}
 		// Classification
 		String serializedClassifier = "dell_first_25_data_ner-model.ser.gz";
 		try {
 			AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
-			List<List<CoreLabel>> out = classifier.classify(tokenized.toString());
-			System.out.println(out);
+			List<CoreLabel> out = classifier.classify(tokens);
+			for(CoreLabel lable: out) {
+				System.out.println("\t" + lable.word());
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
