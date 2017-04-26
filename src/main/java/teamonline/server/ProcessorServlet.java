dd976f8b4ef54cf8126ff12f53d788b6fbad23eb
faran <faran@faran-desktop>
2017-04-25 22:55:32 -0400

    scraping server side

--- src/main/java/teamonline/server/ProcessorServlet.java
+++ src/main/java/teamonline/server/ProcessorServlet.java
@@ -1,11 +1,8 @@
 package teamonline.server;
 
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,13 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import edu.stanford.nlp.classify.Classifier;
-import edu.stanford.nlp.ie.AbstractSequenceClassifier;
-import edu.stanford.nlp.ie.crf.CRFClassifier;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.ling.Word;
-import edu.stanford.nlp.process.PTBTokenizer;
-import edu.stanford.nlp.util.CoreMap;
+import com.jaunt.Element;
+import com.jaunt.Elements;
+import com.jaunt.NotFound;
+import com.jaunt.ResponseException;
+import com.jaunt.UserAgent;
 
 public class ProcessorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -35,37 +30,56 @@ public class ProcessorServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Processing started...");
-		String text = request.getParameter("text");
-		String[] items = text.split(Pattern.quote("||"));
-		StringBuffer tokenized = new StringBuffer();
-		ArrayList<CoreLabel> tokens = new ArrayList<CoreLabel>();
-		
-		// Tokenizing
-		for(String item: items) {
-			item = item.replace('-', ' ');
-			Reader reader = new StringReader(item);
-			PTBTokenizer<Word> tokenizer = PTBTokenizer.newPTBTokenizer(reader);
-			Word nextToken = null;
-			while(tokenizer.hasNext()) {
-				nextToken = tokenizer.next();
-				tokenized.append(nextToken.word());
-				System.out.println(nextToken.word());
-				tokens.add(new CoreLabel(nextToken));
-			}
-		}
-		// Classification
-		String serializedClassifier = "dell_first_25_data_ner-model.ser.gz";
+		String url = request.getParameter("url");
+		String linkClass = request.getParameter("linkClass");
+		UserAgent userAgent = new UserAgent();
+		List<String> titles = new ArrayList<String>();
 		try {
-			AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
-			List<CoreLabel> out = classifier.classify(tokens);
-			for(CoreLabel lable: out) {
-				System.out.println("\t" + lable.word());
-			}
-			
-		} catch (Exception e) {
+			userAgent.visit(url);
+			Elements links = userAgent.doc.findEvery("<a class=\""+linkClass+"\">");
+			for(Element link : links) 
+				titles.add(link.getAt("title")); 
+		} catch (ResponseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotFound e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		request.getSession(true).setAttribute("text", tokenized.toString());
+		
+		request.getSession(true).setAttribute("text", String.join("<br/>", titles));
+		
+//		String text = request.getParameter("text");
+//		String[] items = text.split(Pattern.quote("||"));
+//		StringBuffer tokenized = new StringBuffer();
+//		ArrayList<CoreLabel> tokens = new ArrayList<CoreLabel>();
+//		
+//		// Tokenizing
+//		for(String item: items) {
+//			item = item.replace('-', ' ');
+//			Reader reader = new StringReader(item);
+//			PTBTokenizer<Word> tokenizer = PTBTokenizer.newPTBTokenizer(reader);
+//			Word nextToken = null;
+//			while(tokenizer.hasNext()) {
+//				nextToken = tokenizer.next();
+//				tokenized.append(nextToken.word());
+//				System.out.println(nextToken.word());
+//				tokens.add(new CoreLabel(nextToken));
+//			}
+//		}
+//		// Classification
+//		String serializedClassifier = "dell_first_25_data_ner-model.ser.gz";
+//		try {
+//			AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
+//			List<CoreLabel> out = classifier.classify(tokens);
+//			for(CoreLabel lable: out) {
+//				System.out.println("\t" + lable.word());
+//			}
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		request.getSession(true).setAttribute("text", tokenized.toString());
 	}
 
 }
